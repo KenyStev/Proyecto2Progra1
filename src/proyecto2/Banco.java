b48a16619bd9266aa09aa67cd5f7ed78f263aec2
KenyStev <kjestevez.0795@gmail.com>
2014-11-27 14:02:01 -0600

    Agregado metodo login y logout en banco y sets y gets en CuentaBancaria

--- src/proyecto2/Banco.java
+++ src/proyecto2/Banco.java
@@ -5,10 +5,43 @@
  */
 package proyecto2;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Rolando
+ * @author zerokull
  */
 public class Banco {
+    private Scanner scan = new Scanner(System.in);
+    private CuentaBancaria cuentas[];
+    private Usuario users[];
+    private Usuario activo;
+
+    public Banco() {
+        this.activo = null;
+        cuentas = new CuentaBancaria[200];
+        users = new Usuario[50];
+    }
     
+    public void login(){
+        System.out.print("Ingrese nombre de Usuario: ");
+        String user= scan.next();
+        System.out.print("Ingrese pasword: ");
+        String pass= scan.next();
+        
+        for (Usuario usuario : users) {
+//            if(usuario != null && usuario.getNombre().equals(user) 
+//                    && usuario.getPassword().equals(pass)){
+                activo = usuario;
+                break;
+//            }
+        }
+        if(activo==null){
+            System.out.println("Usuario o contrasenia Incorrecta!");
+        }
+    }
+    
+    public void logout(){
+        activo = null;
+    }
 }
--- src/proyecto2/CuentaBancaria.java
+++ src/proyecto2/CuentaBancaria.java
@@ -7,8 +7,43 @@ package proyecto2;
 
 /**
  *
- * @author Rolando
+ * @author zerokull
  */
 public class CuentaBancaria {
+    private String name, tipo;
+    private int numero;
+    private double tasaInteres, saldo;
+    private boolean activa;
+
+    public CuentaBancaria(String name, String tipo, int numero, double tasaInteres) {
+        this.name = name;
+        this.tipo = tipo;
+        this.numero = numero;
+        this.tasaInteres = tasaInteres;
+        saldo = 500;
+        activa = true;
+    }
     
+    public void addSaldo(double saldo){
+        if(activa){
+         this.saldo+=saldo;   
+        }else{
+            this.saldo=(saldo-(saldo*0.2));
+            activa = true;
+        }
+    }
+    
+    /*Funciones Set*/
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+    
+    /*Funciones Get*/
+    public int getNum(){
+        return numero;
+    }
+    
+    public double getSaldo(){
+        return saldo;
+    }
 }
