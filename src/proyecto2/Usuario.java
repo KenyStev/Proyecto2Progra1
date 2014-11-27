bc93b5abd5d2ce302accd4ac67f7d827125ba6f5
rolandoh <rolandoh@unitec.edu>
2014-11-27 14:19:27 -0600

    Usuario Iniciado

--- src/proyecto2/Usuario.java
+++ src/proyecto2/Usuario.java
@@ -4,11 +4,49 @@
  * and open the template in the editor.
  */
 package proyecto2;
-
+import java.util.Date;
 /**
  *
  * @author Rolando
  */
 public class Usuario {
+    public String email, password, nombre, tipo;
+    public Date fecha;
+    
+    public Usuario(String email, String pass, String nombre, String tipo){
+        this.email = email;
+        this.password = pass;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        fecha = new Date();
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
     
 }
